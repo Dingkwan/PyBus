f0487e6c887aedc93afe66804e9fde019defd205
Dingkwan <72882556+Dingkwan@users.noreply.github.com>
2024-04-15 22:57:52 +0800

    Update GUI_by_sg.py

--- GUI_by_sg.py
+++ GUI_by_sg.py
@@ -1,12 +1,19 @@
 import PySimpleGUI as sg
+import pandas as pd
 
 # All the stuff inside your window.
-layout = [  [sg.Text("What's your name?")],
-            [sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Text("Some bus route", font = ('Arial', 30))],
+            [sg.Button("Bus 1", size = (50,1), font = ("Arial", 20))],
+            [sg.Button("Bus 2", size = (50,1), font = ("Arial", 20))],
+            [sg.Button("Bus 3", size = (50,1), font = ("Arial", 20))],
+            [sg.Button("Bus 4", size = (50,1), font = ("Arial", 20))],
+            [sg.Button("Bus 5", size = (50,1), font = ("Arial", 20))],
+            [sg.Text("Or you can choose a GPS data file to analyse:", font = ("Arial", 15)), sg.Button("File...", font = ("Arial",15))],
+            [sg.Text(key = "open", font = ("Arial", 13))]
+        ]
 
 # Create the Window
-window = sg.Window('Hello Example', layout)
+window = sg.Window('PyBus', layout, element_justification = 'centre', size = (600, 500))
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -15,7 +22,10 @@ while True:
     # if user closes window or clicks cancel
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
-
-    print('Hello', values[0], '!')
+    
+    if event == "File...":
+        file = sg.PopupGetFile("Select file", no_window = True)
+        window["open"].update(file)
+        print(file)
 
 window.close()
